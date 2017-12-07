521e912dec7e0b381c56c942e285edc2fb585582
samuelthomps0n <sam@samuelthompson.co.uk>
2017-12-07 11:14:25 +0000

    Initial Commit
    Create the script that will automatically update all package versions
    based on what is currently being used. Added .env support so that devs
    don't have to keep putting in long CLI options.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # pattern-library-updater
 Node updater for Lerna pattern library versions in to projects
+
+This simple script will read your projects package.json and pull through any new version numbers from a source project. New package still have to be manually added.
+
+## How to use
+
+Make sure that the pattern library project is up to date (checkout master and make sure you've done a pull)
+
+```
+node index.js --project <path to project> --library <path to project> --scope <npm scope>
+```
+
+## .env
+
+You can also add a `.env` file and put the options in there. Copy the `.env-example` file and add your own values.
---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const argv = require('yargs').argv
+const path = require('path')
+const fs   = require('fs')
+const env  = require('dotenv').load()
+
+const projectPath = argv.project || process.env.PROJECT_PATH
+const libPath = argv.library || process.env.LIBRARY_PATH
+const scope = argv.scope || process.env.SCOPE
+
+let projectPackageJSON = require(path.join(projectPath, '/package.json'))
+
+for(var dependency in projectPackageJSON.dependencies){
+    if(dependency.startsWith(scope)) {
+
+    	const packagePath = path.join(libPath, 'packages', dependency.replace(scope + "/", ""), '/package.json')
+
+    	if (fs.existsSync(packagePath)) {
+		    const packageJson = require(packagePath)
+		    console.log("Changing version of " + dependency + ": " + projectPackageJSON.dependencies[dependency] + " -> " + packageJson.version)
+	    	projectPackageJSON.dependencies[dependency] = packageJson.version
+		}
+    }
+}
+
+savePackageJSON(projectPackageJSON, projectPath)
+
+function savePackageJSON(packageJson, projectPath) {
+	packageJson = JSON.stringify(packageJson, null, 2)
+
+	fs.writeFile(path.join(projectPath, '/package.json'), packageJson, function(err) {
+	    if(err) {
+	        return console.log(err)
+	    }
+
+	    console.log("The package.json was update")
+	})
+}
