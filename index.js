7d3113f232e53aaf8253fe8f6b18eac4a4b442cf
samuelthomps0n <sam@samuelthompson.co.uk>
2017-12-13 10:51:25 +0000

    Add --prompt flag
    Add a flag to prompt the user each time a package is going to be updated.
    By default this is set to no, or a user can enter 'y' in the CLI to
    update the package.
    
    Also updated some styling to the confirmation message.

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
-const argv = require('yargs').argv
+// Internal dependencies
 const path = require('path')
 const fs   = require('fs')
-const env  = require('dotenv').load()
 
+// External dependencies
+const env  = require('dotenv').load()
+const argv = require('yargs').argv
+const prompt = require('prompt-sync')({
+	sigint: true
+})
+
+// Env variables
 const projectPath = argv.project || process.env.PROJECT_PATH
 const libPath = argv.library || process.env.LIBRARY_PATH
 const scope = argv.scope || process.env.SCOPE
+const confirm = argv.prompt || false
 
 let projectPackageJSON = require(path.join(projectPath, '/package.json'))
 
-for(var dependency in projectPackageJSON.dependencies){
-	if(dependency.startsWith(scope)) {
+for (var dependency in projectPackageJSON.dependencies){
+	if (dependency.startsWith(scope)) {
 
 		const packagePath = path.join(libPath, 'packages', dependency.replace(scope + "/", ""), '/package.json')
 
 		if (fs.existsSync(packagePath)) {
 			const packageJson = require(packagePath)
-			console.log("Changing version of " + dependency + ": " + projectPackageJSON.dependencies[dependency] + " -> " + packageJson.version)
-			projectPackageJSON.dependencies[dependency] = packageJson.version
+			if (packageJson.version !== projectPackageJSON.dependencies[dependency]) {
+				
+				let ok = 'y'
+
+				if (confirm) {
+					ok = prompt('Update ' + dependency + ' from ' + projectPackageJSON.dependencies[dependency] + ' to ' + packageJson.version + '? (y/n) ', 'n')
+				}
+
+				if (ok === 'y') {
+					updatePackage(dependency, packageJson)
+				} else {
+					console.log("Skipping " + dependency)
+				}
+			}
 		}
 	}
 }
 
 savePackageJSON(projectPackageJSON, projectPath)
+
+/**
+ * @param  {string} dependency
+ * @param  {Object} packageJson 
+ */
+function updatePackage(dependency, packageJson) {
+	console.log("\x1b[32m\u2713\x1b[0m ",dependency + "...", projectPackageJSON.dependencies[dependency], "->", packageJson.version)
+	projectPackageJSON.dependencies[dependency] = packageJson.version
+}
 
 /**
  * @param  {Object} packageJSON
@@ -32,7 +61,7 @@ function savePackageJSON(packageJson, projectPath) {
 	packageJson = JSON.stringify(packageJson, null, 2)
 
 	fs.writeFile(path.join(projectPath, '/package.json'), packageJson, function(err) {
-		if(err) {
+		if (err) {
 			return console.log(err)
 		}
 
